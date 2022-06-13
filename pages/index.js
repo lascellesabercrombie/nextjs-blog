ded3d6f7e2eb726a973de1b9e88dad21c5f4ef6b
Alex Grafen <alex.grafen@gmail.com>
2022-06-13 15:14:28 +0100

    arbitrary changes

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm Alex.</p>
+        <p>Welcome to this test site.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
